5868911797aef5daa74f72872316070791f8540e
sjoe97Github <sjoe97@yahoo.com>
2024-11-20 14:25:27 -0700

    introduce docker compose configuration allowing node service to connect to mysql on local machine, not in a container

--- src/dal/dbconnection.ts
+++ src/dal/dbconnection.ts
@@ -1,10 +1,14 @@
 import mysql from 'mysql2/promise';
 
 console.log('Running with NODE_ENV:', process.env.NODE_ENV);
+console.log('Running with DB_HOST:', process.env.DB_HOST);
+console.log('Running with DB_USER:', process.env.DB_USER);
+console.log('Running with DB_PASSWORD:', process.env.DB_PASSWORD);
+console.log('Running with DB_NAME:', process.env.DB_NAME);
 
 export const db = mysql.createPool({
-    host: 'localhost',
-    user: 'root',
-    password: '***REMOVED***',
-    database: 'employees',
+    host: process.env.DB_HOST || 'localhost',
+    user: process.env.DB_USER || 'yourusername',
+    password: process.env.DB_PASSWORD || 'yourpassword',
+    database: process.env.DB_NAME || 'employees',
 });
